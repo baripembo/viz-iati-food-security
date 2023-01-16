d794ae979ad406bdbc0cc200306661975e4c35dd
baripembo <erikawei@gmail.com>
2023-01-16 18:27:55 -0500

    fix filtering bug

--- pages/index.vue
+++ pages/index.vue
@@ -441,15 +441,15 @@ export default {
       return this.getRankedList(this.spending)
     },
     orgCount () {
-      const orgs = [...new Set(this.fullData.map(item => item['#org+id']))]
+      const orgs = [...new Set(this.filteredData.map(item => item['#org+id']))]
       return orgs.length
     },
     countryCount () {
-      const countries = [...new Set(this.fullData.map(item => item['#country']))]
+      const countries = [...new Set(this.filteredData.map(item => item['#country']))]
       return countries.length
     },
     sectorCount () {
-      const sector = [...new Set(this.fullData.map(item => item['#sector']))]
+      const sector = [...new Set(this.filteredData.map(item => item['#sector']))]
       return sector.length
     },
     activityCount () {
@@ -540,8 +540,6 @@ export default {
         this.orgNameIndex = response.data.data
         this.$store.commit('setOrgNameIndex', response.data.data)
 
-        // this.selectedFilterLabel = this.orgNameIndex.length + ' publishing organizations'
-
         this.$nextTick(() => {
           if ('org' in this.$route.query) {
             this.filterParams['#org+id'] = this.$route.query.org
@@ -602,9 +600,10 @@ export default {
           this.filteredData = this.filterData()
 
           // set intitial filter label
-          if (Object.keys(this.$route.query).length < 1) {
-            const count = this.orgCount
-            this.selectedFilterLabel = count + ' publishing organizations'
+          if (this.selectedFilter !== '*') {
+            this.selectedFilterLabel = (this.selectedFilterDimension === '#org+id') ? this.getOrgName(this.selectedFilter) : this.selectedFilter
+          } else {
+            this.setDefaultFilterLabel(this.selectedFilterDimension)
           }
         })
     },
@@ -671,6 +670,12 @@ export default {
     onToggle (event) {
       this.filterParams[event.target.parentElement.id] = event.target.value
       this.updateFilteredData()
+
+      if (this.selectedFilter !== '*') {
+        this.selectedFilterLabel = (this.selectedFilterDimension === '#org+id') ? this.getOrgName(this.selectedFilter) : this.selectedFilter
+      } else {
+        this.setDefaultFilterLabel(this.selectedFilterDimension)
+      }
       // this.$mixpanelTrackAction('change content', 'Commitments and Spending Breakdown toggle filter', event.target.parentElement.id + ' ' + event.target.value)
     },
     onSelectRanking (value) {
